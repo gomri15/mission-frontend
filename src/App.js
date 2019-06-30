c79d26345ceec7e5a67005c6fb211ae83e8fcfe4
Omri Golan <gomri15@gmail.com>
2019-06-30 23:35:32 +0300

    Start out

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "./components/Register";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
